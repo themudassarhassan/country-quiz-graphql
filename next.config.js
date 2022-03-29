6f5b027f8c9c65bee8484c85aa23141164799189
Mudassar <mudassarhassan135@gmail.com>
2022-03-29 14:33:03 +0500

    added GTM

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  script-src 'self' '${process.env.NODE_ENV === 'development' ? 'unsafe-eval' : ''}' 'unsafe-inline' https://www.googletagmanager.com;
+  font-src 'self';  
+`;
+
+const securityHeaders = [
+  // This response header converts HTTP requests to HTTPS which prevents man-in-the-middle attack
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
 import { useRouter } from 'next/router';
+import { GTMProvider } from '@elgorditosalsero/react-gtm-hook';
 import type { AppProps } from 'next/app';
 import client from '../appolo-client';
 import '../styles/globals.css';
@@ -7,6 +8,8 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const isClientSide = router.pathname.includes('client-side');
+  const gtmParams = { id: 'GTM-MSCKRX9' };
+  
   
   if (isClientSide) {
     return (
@@ -16,7 +19,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     );
   }
   
-  return <Component {...pageProps} />;
+  return (
+    <GTMProvider state={gtmParams}>
+      <Component {...pageProps} />    
+    </GTMProvider>
+  );
 }
 
 export default MyApp;
